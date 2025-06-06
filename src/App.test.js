f8e93d02cf4093e0b1467fdfae96d976a76d42b0
Sushrut117 <sharmasushrut7@gmail.com>
2025-06-06 06:13:37 +0000

    Added new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders GitHub repo link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/view github repository/i);
   expect(linkElement).toBeInTheDocument();
 });
+
